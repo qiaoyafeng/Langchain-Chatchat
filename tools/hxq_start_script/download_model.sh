4f039e986efc7bd8df6e1921a55f6ce0537b134d
qiaoyafeng <qiaoyafeng@gmail.com>
2024-08-24 12:50:29 +0800

    add hxq startup shell

---
+++ tools/hxq_start_script/download_model.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# 模型文件已在服务器上：路径为 /opt/python/gml_model/glm-4-9b-chat-int4
+# cd /root/autodl-tmp && conda run -n base pip install codewithgpu && conda run -n base cg down xxxiu/glm-4-9b-chat
